cd80aef493cc04b076f08c30e7f154857118efaf
orlapez <orlapez@gmail.com>
2023-09-15 07:58:33 -0500

    Countries migration

---
+++ Market/Market.API/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Market.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Market.API.Data
+{
+    public class DataContext:DbContext
+    {
+
+        //Utilizar las propiedades y opciones de DBContext
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+
+
+
+    }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+
+
+}
---
+++ Market/Market.Shared/Entities/Country.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Shared.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }// primary key, identity(1,1)
+
+        [Display (Name="Pais")] // Son etiquetas del nombre del campo
+       [MaxLength (100,ErrorMessage ="El campo {0} debe contener únicamente 100 caracteres")] // Es la longitud de caracteres del campo}
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public string? Name { get; set; }    // ?(indica que hace un salto de nulos)
+        
+    }
+}
